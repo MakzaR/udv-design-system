eccd66ad31cfba669e05cef2d7fa11223d69d2e3
MakzaR <makzarchukov@gmail.com>
2021-07-26 03:34:46 +0500

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,52 @@
-import React from "react";
-import {NavLink} from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import {NavLink, useLocation} from "react-router-dom";
 
-import styles from "./Navbar.module.css"
+import "./Navbar.css"
 
 export default function Navbar() {
+    const location = useLocation();
+    const {pathname} = location;
+    const currentPage = pathname.split("/")[1];
+
+    console.log(document.documentElement.className)
+
     return (
-        <div>
-            <ul>
-                <li className="menu_item">
-                    <NavLink to='/home' activeClassName={styles.activeLink}>
-                        <div className={styles.logo}>UDV</div>
-                    </NavLink>
-                    Дизайн-система
-                </li>
-                <li className="menu_item">
-                    <NavLink to='/components' activeClassName={styles.activeLink}>Компоненты</NavLink>
-                </li>
-                <li className="menu_item">
-                    <NavLink to='/patterns'  activeClassName={styles.activeLink}>Паттерны</NavLink>
-                </li>
-                <li className="menu_item">
-                    <NavLink to='/ux' activeClassName={styles.activeLink}>Принципы UX</NavLink>
-                </li>
-                <li className="menu__item">
-                    <NavLink to='/research' activeClassName={styles.activeLink}>Исследования</NavLink>
-                </li>
-                <li className="menu__item">
-                    <NavLink to='/articles' activeClassName={styles.activeLink}>Статьи</NavLink>
-                </li>
-            </ul>
+        <div className={currentPage === "components" ? "navbar grey" : "navbar"}>
+            <div className="name">
+                <NavLink to='/home' className="logo">UDV</NavLink>
+                <span className="system_name">Дизайн-система</span>
+            </div>
+            <div className="menu">
+                <NavLink to='/components' className="item"
+                         activeClassName="activeLink components">Компоненты</NavLink>
+                <NavLink to='/patterns' className="item"
+                         activeClassName="activeLink">Паттерны</NavLink>
+                <NavLink to='/ux' className="item"
+                         activeClassName="activeLink">Принципы UX</NavLink>
+                <NavLink to='/research' className="item"
+                         activeClassName="activeLink">Исследования</NavLink>
+                <NavLink to='/articles' className="item"
+                         activeClassName="activeLink">Статьи</NavLink>
+            </div>
         </div>
+
+        // <div className={styles.navbar}>
+        //     <div className={styles.name}>
+        //         <NavLink to='/home' className={styles.logo}>UDV</NavLink>
+        //         <span className={styles.system_name}>Дизайн-система</span>
+        //     </div>
+        //     <div className={styles.menu}>
+        //         <NavLink to='/components' className={styles.item}
+        //                  activeClassName={styles.activeLink}>Компоненты</NavLink>
+        //         <NavLink to='/patterns' className={styles.item}
+        //                  activeClassName={styles.activeLink}>Паттерны</NavLink>
+        //         <NavLink to='/ux' className={styles.item}
+        //                  activeClassName={styles.activeLink}>Принципы UX</NavLink>
+        //         <NavLink to='/research' className={styles.item}
+        //                  activeClassName={styles.activeLink}>Исследования</NavLink>
+        //         <NavLink to='/articles' className={styles.item}
+        //                  activeClassName={styles.activeLink}>Статьи</NavLink>
+        //     </div>
+        // </div>
     )
 }
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../Navbar";
 
+import styles from "./Home.module.css"
+
 export default function Home() {
 
     return (
-        <div>
+        <div className={styles.dark_background}>
             <Navbar/>
             Главная страница
         </div>
