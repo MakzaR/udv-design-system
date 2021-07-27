cb5d96951f66da22752bd4ac39eaaf9d8ff93c09
MakzaR <makzarchukov@gmail.com>
2021-07-28 03:17:17 +0500

    testing heroku

--- src/components/pages/Research/Research.js
+++ src/components/pages/Research/Research.js
@@ -1,12 +1,20 @@
 import React from "react";
+
 import Navbar from "../../Navbar";
 
-export default function Research() {
+import styles from "./Research.module.css";
 
+export default function Research() {
     return (
-        <div>
-            <Navbar/>
-            Исследования
+        <div className={styles.research_page}>
+            <Navbar className={styles.navbar}/>
+            <div className={styles.line_dark}/>
+            <div className={styles.research_content}>
+                <div className={styles.heading}>
+                    Исследования
+                </div>
+
+            </div>
         </div>
     )
 }
