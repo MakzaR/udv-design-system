086ef3b9070a68a34370ad2a2deb537b2772bcb8
MakzaR <makzarchukov@gmail.com>
2021-07-28 13:48:04 +0500

    finished styling research page

--- src/components/pages/Research/Research.js
+++ src/components/pages/Research/Research.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {ReactSVG} from "react-svg";
+
 
 import Navbar from "../../Navbar";
 import Table from "./Table/Table";
 
 import styles from "./Research.module.css";
+
+import one from "../../../assets/one.svg";
+import two from "../../../assets/two.svg";
+import three from "../../../assets/three.svg";
 
 export default function Research() {
     return (
@@ -23,11 +29,38 @@ export default function Research() {
                     <div>
                         Ниже мы рассказываем об исследованиях, которые мы проводим - когда и какое выбрать и на каком
                         этапе это можно сделать. С общими рекомендациями об исследованиях можно
-                        ознакомиться <a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">здесь</a>.
+                        ознакомиться <a target="_blank" rel="noreferrer"
+                                        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">здесь</a>.
                     </div>
                 </div>
-                <div  className={styles.table}>
+                <div className={styles.table}>
                     <Table/>
+                </div>
+                <div className={styles.note}>
+                    <div className={styles.noteItem}>
+                        <ReactSVG src={one} wrapper="span" className={styles.num}/>
+                        <span>
+                            Аналитика
+                            <br/>
+                            (сбор и формулирование требований)
+                        </span>
+                    </div>
+                    <div className={styles.noteItem}>
+                        <ReactSVG src={two} wrapper="span" className={styles.num}/>
+                        <span>
+                            Проектирование
+                            <br/>
+                            (дизайн и разработка)
+                        </span>
+                    </div>
+                    <div className={styles.noteItem}>
+                        <ReactSVG src={three} wrapper="span" className={styles.num}/>
+                        <span>
+                            Релиз
+                            <br/>
+                            (поддержка и развитие)
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
