1cde255de20e74f73a7a72a066334c0484668dc9
MakzaR <makzarchukov@gmail.com>
2021-09-20 10:37:02 +0500

    added Components page

--- src/components/pages/ReactComponents/Components.js
+++ src/components/pages/ReactComponents/Components.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './Components.module.css';
+import styles from './Components.module.css';
 
 import Navbar from "../../Navbar";
 
@@ -9,26 +9,26 @@ import copy from "./copy.svg";
 
 export default function Components() {
     return (
-        <div>
-            <Navbar/>
-            <div className={classes.container}>
-                <div className={classes.left_menu}>
-                    <input type="search" placeholder="Поиск" className={classes.search}/>
-                    <div className={classes.vertmenu}>
+        <div className={styles.react_page}>
+            <Navbar className={styles.navbar}/>
+            <div className={styles.react_content}>
+                <div className={styles.left_menu}>
+                    <input type="search" placeholder="Поиск" className={styles.search}/>
+                    <div className={styles.vertmenu}>
                         <ul>
                             <li>
 
-                                <a className={classes.active, classes.menu_title} href="#">Атомы
+                                <a className={styles.menu_title} href="#">Атомы
                                 </a>
-                                <input type="checkbox" className={classes.subCat} id={1} defaultChecked="checked"/>
-                                <label className={classes.chka} htmlFor={1}/>
+                                <input type="checkbox" className={styles.subCat} id={1} defaultChecked="checked"/>
+                                <label className={styles.chka} htmlFor={1}/>
 
                                 <ul style={{display: 'none'}}>
                                     <li>
-                                        <a href="#" className={classes.menu_sub_title}>Кнопки</a>
-                                        <input type="checkbox" className={classes.subCat} id="1.1"
+                                        <a href="#" className={styles.menu_sub_title}>Кнопки</a>
+                                        <input type="checkbox" className={styles.subCat} id="1.1"
                                                defaultChecked="checked"/>
-                                        <img className={classes.chka} htmlFor="1.1"/>
+                                        <img className={styles.chka} htmlFor="1.1"/>
                                         <ul>
                                             <li>
                                                 <div style={{
@@ -36,62 +36,62 @@ export default function Components() {
                                                     margin: '0 -28px',
                                                     padding: '0 28px'
                                                 }}>
-                                                    <a className={classes.menu_options} href="#">MainButton</a>
+                                                    <a className={styles.menu_options} href="#">MainButton</a>
                                                 </div>
 
                                             </li>
                                             <li>
-                                                <a className={classes.menu_options} href="#">SecondaryButton</a>
+                                                <a className={styles.menu_options} href="#">SecondaryButton</a>
                                             </li>
                                             <li>
-                                                <a className={classes.menu_options} href="#">Link</a>
+                                                <a className={styles.menu_options} href="#">Link</a>
                                             </li>
                                             <li>
-                                                <a className={classes.menu_options} href="#">IconButton</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#" className={classes.menu_sub_title}>Поля</a>
-                                        <input type="checkbox" className={classes.subCat} id="1.1"
-                                               defaultChecked="checked"/>
-                                        <ul>
-                                            <li>
-                                                <a className={classes.menu_options} href="#">Input</a>
-                                            </li>
-                                            <li>
-                                                <a className={classes.menu_options} href="#">FileInput</a>
-                                            </li>
-                                            <li>
-                                                <a className={classes.menu_options} href="#">Select</a>
-                                            </li>
-                                            <li>
-                                                <a className={classes.menu_options} href="#">MultiSelect</a>
-                                            </li>
-                                            <li>
-                                                <a className={classes.menu_options} href="#">DateInput</a>
+                                                <a className={styles.menu_options} href="#">IconButton</a>
                                             </li>
                                         </ul>
                                     </li>
                                     <li>
-                                        <a href="#" className={classes.menu_sub_title}>Поля</a>
-                                        <input type="checkbox" className={classes.subCat} id="1.1"
+                                        <a href="#" className={styles.menu_sub_title}>Поля</a>
+                                        <input type="checkbox" className={styles.subCat} id="1.1"
                                                defaultChecked="checked"/>
                                         <ul>
                                             <li>
-                                                <a className={classes.menu_options} href="#">Input</a>
+                                                <a className={styles.menu_options} href="#">Input</a>
                                             </li>
                                             <li>
-                                                <a className={classes.menu_options} href="#">FileInput</a>
+                                                <a className={styles.menu_options} href="#">FileInput</a>
                                             </li>
                                             <li>
-                                                <a className={classes.menu_options} href="#">Select</a>
+                                                <a className={styles.menu_options} href="#">Select</a>
                                             </li>
                                             <li>
-                                                <a className={classes.menu_options} href="#">MultiSelect</a>
+                                                <a className={styles.menu_options} href="#">MultiSelect</a>
                                             </li>
                                             <li>
-                                                <a className={classes.menu_options} href="#">DateInput</a>
+                                                <a className={styles.menu_options} href="#">DateInput</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#" className={styles.menu_sub_title}>Поля</a>
+                                        <input type="checkbox" className={styles.subCat} id="1.1"
+                                               defaultChecked="checked"/>
+                                        <ul>
+                                            <li>
+                                                <a className={styles.menu_options} href="#">Input</a>
+                                            </li>
+                                            <li>
+                                                <a className={styles.menu_options} href="#">FileInput</a>
+                                            </li>
+                                            <li>
+                                                <a className={styles.menu_options} href="#">Select</a>
+                                            </li>
+                                            <li>
+                                                <a className={styles.menu_options} href="#">MultiSelect</a>
+                                            </li>
+                                            <li>
+                                                <a className={styles.menu_options} href="#">DateInput</a>
                                             </li>
                                         </ul>
                                     </li>
@@ -99,67 +99,68 @@ export default function Components() {
                             </li>
                         </ul>
                     </div>
-                    <div className={classes.vertmenu}>
+                    <div className={styles.vertmenu}>
                         <ul>
                             <li>
-                                <a className={classes.active, classes.menu_title} href="#">Молекулы
+                                <a className={styles.menu_title} href="#">Молекулы
                                 </a>
-                                <input type="checkbox" className={classes.subCat} id={2}/>
-                                <label className={classes.chka} htmlFor={2}/>
+                                <input type="checkbox" className={styles.subCat} id={2}/>
+                                <label className={styles.chka} htmlFor={2}/>
                                 <ul>
                                     <li>
-                                        <a href="#">Пункт меню 2.1</a>
+                                        <a className={styles.menu_sub_title} href="#">Пункт меню 2.1</a>
                                     </li>
                                     <li>
-                                        <a href="#">Пункт меню 2.2</a>
+                                        <a className={styles.menu_sub_title} href="#">Пункт меню 2.2</a>
                                     </li>
                                     <li>
-                                        <a href="#">Пункт меню 2.3</a>
+                                        <a className={styles.menu_sub_title} href="#">Пункт меню 2.3</a>
                                     </li>
                                     <li>
-                                        <a href="#">Пункт меню 2.4</a>
+                                        <a className={styles.menu_sub_title} href="#">Пункт меню 2.4</a>
                                     </li>
                                 </ul>
                             </li>
                         </ul>
                     </div>
                 </div>
-                <div className={classes.content}>
-                    <div className={classes.title}>MainButton</div>
+                <div className={styles.content}>
+                    <div className={styles.title}>MainButton</div>
                     <div style={{display: 'flex'}}>
-                        <p className={classes.text}>import Amount from 'arui-feather/amount';</p>
-                        <img src={copy} className={classes.img_copy}/>
+                        <p className={styles.text}>import Amount from 'arui-feather/amount';</p>
+                        <img src={copy} className={styles.img_copy}/>
                     </div>
-                    <div className={classes.sub_title}>Компонент</div>
-                    <div className={classes.button}>Главная кнопка</div>
-                    <div className={classes.line}/>
-                    <div className={classes.code_title}><span
-                        style={{color: '#00B98A', textDecoration: 'underline'}}>Код</span> <span
+                    <div className={styles.sub_title}>Компонент</div>
+                    <div className={styles.button}>Главная кнопка</div>
+                    <div className={styles.line}/>
+                    <div className={styles.code_title}><span
+                        className={styles.code_top}>Код</span> <span
                         style={{marginLeft: '28px'}}>Свойства и методы</span></div>
-                    <div className={classes.code_block}>
-                            <pre className={classes.code}>{"    "}<span className={classes.blue}>const</span> <span
-                                className={classes.purple}>AMOUNT</span> ={"{"}<br/>{"\n"}{"        "}value <span
-                                className={classes.purple}>123535</span>,<br/>{"\n"}{"        "}currency: {"{"}<br/>{"\n"}{"            "}code: <span
-                                className={classes.green}>'RUR'</span>,<br/>{"\n"}{"            "}minority: <span
-                                className={classes.purple}>100</span><br/>{"\n"}{"        "}{"}"}{"\n"}{"    "}{"}"};{"\n"}{"    "}<br/>{"\n"}{"    "}&lt;
-                                <span className={classes.purple}>div</span>&gt;
+                    <div className={styles.code_block}>
+                            <pre className={styles.code}>{"    "}<span className={styles.blue}>const</span> <span
+                                className={styles.purple}>AMOUNT</span> ={"{"}<br/>{"\n"}{"        "}value <span
+                                className={styles.purple}>123535</span>,<br/>{"\n"}{"        "}currency: {"{"}<br/>{"\n"}{"            "}code: <span
+                                className={styles.green}>'RUR'</span>,<br/>{"\n"}{"            "}minority: <span
+                                className={styles.purple}>100</span><br/>{"\n"}{"        "}{"}"}{"\n"}{"    "}{"}"};{"\n"}{"    "}<br/>{"\n"}{"    "}&lt;
+                                <span className={styles.purple}>div</span>&gt;
                                 <br/>{"\n"}{"        "}{"{"}<br/>{"\n"}{"            "}[<span
-                                    className={classes.green}>'s'</span>, <span
-                                    className={classes.green}>'m'</span>, <span
-                                    className={classes.green}>'1'</span>, <span
-                                    className={classes.green}>'xl'</span>].<span className={classes.red}>map</span>(size =&gt;
-                                <br/>{"\n"}{"                "}&lt;<span className={classes.purple}>div</span> <span
-                                    className={classes.green}>key</span>={"{"} <span
-                                    className={classes.purple}>size</span> {"}"}&gt;
+                                    className={styles.green}>'s'</span>, <span
+                                    className={styles.green}>'m'</span>, <span
+                                    className={styles.green}>'1'</span>, <span
+                                    className={styles.green}>'xl'</span>].<span
+                                    className={styles.red}>map</span>(size =&gt;
+                                <br/>{"\n"}{"                "}&lt;<span className={styles.purple}>div</span> <span
+                                    className={styles.green}>key</span>={"{"} <span
+                                    className={styles.purple}>size</span> {"}"}&gt;
                                 <br/>{"\n"}{"                    "}&lt;<span
-                                    className={classes.red}>Amount</span><br/>{"\n"}{"                        "}<span
-                                    className={classes.green}>size</span>={"{"} <span
-                                    className={classes.purple}>size</span> {"}"}<br/>{"\n"}{"                        "}<span
-                                    className={classes.green}>amount</span>={"{"} <span
-                                    className={classes.purple}>AMOUNT</span> {"}"}<br/>{"\n"}{"                    "}/&gt;
-                                <br/>{"\n"}{"                "}&lt;/<span className={classes.purple}>div</span>&gt;
+                                    className={styles.red}>Amount</span><br/>{"\n"}{"                        "}<span
+                                    className={styles.green}>size</span>={"{"} <span
+                                    className={styles.purple}>size</span> {"}"}<br/>{"\n"}{"                        "}<span
+                                    className={styles.green}>amount</span>={"{"} <span
+                                    className={styles.purple}>AMOUNT</span> {"}"}<br/>{"\n"}{"                    "}/&gt;
+                                <br/>{"\n"}{"                "}&lt;/<span className={styles.purple}>div</span>&gt;
                                 <br/>{"\n"}{"            "}))<br/>{"\n"}{"        "}{"}"}<br/>{"\n"}{"    "}&lt;/<span
-                                    className={classes.purple}>div</span>&gt;<br/>{"\n"}{"    "}</pre>
+                                    className={styles.purple}>div</span>&gt;<br/>{"\n"}{"    "}</pre>
                     </div>
                 </div>
             </div>
